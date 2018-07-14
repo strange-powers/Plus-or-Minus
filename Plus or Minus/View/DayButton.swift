5e8fee50a91830f2190e04305efde8b1f0a95786
Julian Voest <jv@strange-powers.com>
2018-07-14 21:33:04 +0200

    created Initialiser with name of the day the button should present

--- Plus or Minus/View/DayButton.swift
+++ Plus or Minus/View/DayButton.swift
@@ -9,9 +9,20 @@
 import UIKit
 
 class DayButton: UIButton {
-    private let TINT_COLOR = UIColor.white
-    private let BACKGROUND_COLOR = UIColor.green
-    private let FONT_SIZE:CGFloat = 32.0
+    /// The buttons font color
+    static let TINT_COLOR = UIColor.white
+    
+    /// The buttons background color
+    static let BACKGROUND_COLOR = UIColor.green
+    
+    /// The buttons font size
+    static let FONT_SIZE: CGFloat = 32.0
+    
+    /// The buttons width
+    static let WIDTH: CGFloat = 200
+    
+    /// The buttons height
+    static let HEIGHT: CGFloat = 200
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -19,16 +30,32 @@ class DayButton: UIButton {
         styleButton()
     }
     
+    /**
+     Initializes the DayButton object with the name of the day and the default sizes
+     
+     - Parameters
+        - name: The name of the day the button represents on view level
+     */
+    convenience init(withDayName name: String) {
+        let frame = CGRect(x: 0, y: 0, width: DayButton.WIDTH, height: DayButton.WIDTH)
+        self.init(frame: frame)
+        
+        setTitle(name, for: .normal)
+    }
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
+    /**
+     Styles the UIButton
+    */
     private func styleButton() {
         UIView.makeCircleFrom(self)
-        backgroundColor = BACKGROUND_COLOR
-        tintColor = TINT_COLOR
+        backgroundColor = DayButton.BACKGROUND_COLOR
+        tintColor = DayButton.TINT_COLOR
         layer.borderWidth = 0
-        titleLabel?.font = UIFont.boldSystemFont(ofSize: FONT_SIZE)
+        titleLabel?.font = UIFont.boldSystemFont(ofSize: DayButton.FONT_SIZE)
     }
 
 }
