65e1942e03fbf97ada36774a9f372da0c3f8ff01
Julian Voest <jv@strange-powers.com>
2018-07-20 22:44:44 +0200

    created a constants file

---
+++ Plus or Minus/Utils/constants.swift
@@ -0,0 +1,13 @@
+//
+//  constants.swift
+//  Plus or Minus
+//
+//  Created by Julian Vöst on 20.07.18.
+//  Copyright © 2018 Julian Vöst. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let application = UIApplication.shared.delegate as! AppDelegate
+let context = application.persistentContainer.viewContext
