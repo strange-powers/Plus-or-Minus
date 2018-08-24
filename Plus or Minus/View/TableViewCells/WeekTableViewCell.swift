62d2f91b15fb17eb7dc7388ec836c30fc95e4e73
Julian Voest <jv@strange-powers.com>
2018-08-24 20:13:20 +0200

    styled the day format of the cells

---
+++ Plus or Minus/View/TableViewCells/WeekTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  WeekTableViewCell.swift
+//  Plus or Minus
+//
+//  Created by Julian Vöst on 21.08.18.
+//  Copyright © 2018 Julian Vöst. All rights reserved.
+//
+
+import UIKit
+
+class WeekTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+    func prepareCellWithData(from week: [Day]) {
+        if week.count == 7 {
+            let firstDate = formatDate(week.first!.date)
+            let lastDate = formatDate(week.last!.date)
+            
+            label.text = "\(firstDate) - \(lastDate)"
+        } else {
+            label.text = "Something went wrong..."
+        }
+    }
+    
+    private func formatDate(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        let dateTemplate = "yMMMMd"
+        let format = DateFormatter.dateFormat(fromTemplate: dateTemplate, options: 0, locale: .current)!
+        formatter.dateFormat = format
+        let formatedDate = formatter.string(from: date)
+        
+        return formatedDate
+    }
+
+}
