e507489b9b1dddbb8a774a1afe018521d6f3cc08
Julian Voest <jv@strange-powers.com>
2018-09-09 16:45:04 +0200

    created a weekday view controller which lets the user compare the weekdays

---
+++ Plus or Minus/CoreDataController/DayActionController/WeekDayDayAction.swift
@@ -0,0 +1,103 @@
+//
+//  WeekDayDayAction.swift
+//  Plus or Minus
+//
+//  Created by Julian Vöst on 07.09.18.
+//  Copyright © 2018 Julian Vöst. All rights reserved.
+//
+
+import Foundation
+
+class WeekDayDayAction: Rateable {
+    
+    private var _dayName: Calendar.DayName!
+    private var _goodProportion: Float = 0
+    private var _badProportion: Float = 0
+    
+    init(dayName: Calendar.DayName) {
+        _dayName = dayName
+    }
+    
+    var goodProportion: Float {
+        return _goodProportion
+    }
+    
+    var badProportion: Float {
+        return _badProportion
+    }
+    
+    /**
+     Calculates the Proportions of the good and bad day actions which where added between two dates
+     
+     - Parameters:
+         - date1: start point
+         - date2: end point
+     */
+    public func calculateProportions(between date1: Date, and date2: Date) {
+            let dayActions = gatherDayAction(of: _dayName, from: date1, to: date2)
+        print(dayActions)
+            setProportions(for: dayActions)
+    }
+    
+    /**
+     Calculates the Proportions of the good and bad day actions which where added in the given year
+     
+     - Parameters:
+        - year: the year in which the day actions reference to
+     */
+    public func calculateProportions(for year: Int) {
+        let currentCalendar = Calendar.current
+        let askedYearcomponent = DateComponents(calendar: Calendar.current, timeZone: TimeZone.current, era: nil, year: year, month: 1, day: 1, hour: 12)
+        let nextYearcomponent = DateComponents(calendar: Calendar.current, timeZone: TimeZone.current, era: nil, year: year + 1, month: 1, day: 1, hour: 12)
+        
+        if let firstDay = currentCalendar.date(from: askedYearcomponent) {
+            if let firstDayOfNextYear = currentCalendar.date(from: nextYearcomponent) {
+                if let lastDay = currentCalendar.date(byAdding: .day, value: -1, to: firstDayOfNextYear) {
+                    let dayActions = gatherDayAction(of: _dayName, from: firstDay, to: lastDay)
+                    setProportions(for: dayActions)
+                }
+            }
+        }
+    }
+    
+    /**
+     Sets the proportions for the given day actions
+     
+     - Parameters:
+        - dayActions: the day actions on which the calculation is based
+    */
+    private func setProportions(for dayActions: [DayAction]) {
+        let goodDayActions = dayActions.compactMap { (action) -> DayAction? in
+            if !action.conclusion {
+                return action
+            }
+    
+            return nil
+        }
+    
+        _goodProportion = Float(goodDayActions.count) / Float(dayActions.count)
+        _badProportion = 1 - _goodProportion
+    }
+    
+    /**
+     Gathers day actions based on the dayname and a time ranche
+     
+     - Parameters:
+         - day: the day name
+         - date1: start date
+         - date2: end date
+     
+     - Returns: the found day actions as an Array
+     */
+    private func gatherDayAction(of day: Calendar.DayName, from date1: Date, to date2: Date) -> [DayAction] {
+        var actions = [DayAction]()
+        
+        let days = Calendar.current.getWeekDays(between: date1, and: date2, ofType: day, in: TimeZone.current)
+        let dayActionContoller = DayActionController()
+        dayActionContoller.loadActionsBy(week: days)
+        actions = dayActionContoller.dayActions
+        
+        return actions
+    }
+    
+}
--- Plus or Minus/ViewController/CompareDaysViewController.swift
+++ Plus or Minus/ViewController/CompareDaysViewController.swift
@@ -8,17 +8,12 @@
 
 import UIKit
 
-class CompareDaysViewController: UIViewController, Rateable {
+class CompareDaysViewController: UIViewController {
     
     @IBOutlet weak var scrollView: DaysScrollView!
     
-    var goodProportion: Float {
-        return 1
-    }
-    
-    var badProportion: Float {
-        return 1
-    }
+    private var _goodProportion: Float = 0
+    private var _badProportion: Float = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,15 +23,31 @@ class CompareDaysViewController: UIViewController, Rateable {
     }
     
     private func createDayButtons() -> [DayButton] {
-        let dayNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
         var dayButtons = [DayButton]()
         
-        for name in dayNames {
-            let dayButton = DayButton()
-            dayButton.setTitle(name, for: .normal)
-            dayButton.setBackgroundColor(by: self)
+        if let todayLastYear = Calendar.current.date(byAdding: .day, value: -365, to: Date()) {
+            let dayNamesStr = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+            let dayNamesEn = [
+                Calendar.DayName.Monday,
+                Calendar.DayName.Tuesday,
+                Calendar.DayName.Wednesday,
+                Calendar.DayName.Thursday,
+                Calendar.DayName.Friday,
+                Calendar.DayName.Saturday,
+                Calendar.DayName.Sunday
+            ]
             
-            dayButtons.append(dayButton)
+            for index in 0..<dayNamesStr.count {
+                let weekDayDayActionController = WeekDayDayAction(dayName: dayNamesEn[index])
+                let dayButton = DayButton()
+                
+                weekDayDayActionController.calculateProportions(between: Date(), and: todayLastYear)
+                dayButton.setBackgroundColor(by: weekDayDayActionController)
+                dayButton.setTitleWithInitials(title: dayNamesStr[index])
+                
+                dayButtons.append(dayButton)
+            }
+            
         }
         
         return dayButtons
